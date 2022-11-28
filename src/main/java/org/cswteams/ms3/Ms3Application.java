62ea0f50e95f45c1031d2351bdf66b7978f517d0
Torkin <32265040+Torkin1@users.noreply.github.com>
2022-11-28 09:44:08 +0100

    removed unused imports

--- src/main/java/org/cswteams/ms3/Ms3Application.java
+++ src/main/java/org/cswteams/ms3/Ms3Application.java
@@ -2,8 +2,6 @@ package org.cswteams.ms3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Ms3Application {
