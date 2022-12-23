a7d3d16325ffca4bdfa5eb094754cbbe3b549291
Torkin <32265040+Torkin1@users.noreply.github.com>
2022-12-23 22:33:54 +0100

    close #63

--- frontend/src/API/AssegnazioneTurnoAPI.js
+++ frontend/src/API/AssegnazioneTurnoAPI.js
@@ -1,33 +1,48 @@
 export  class AssegnazioneTurnoAPI {
 
-    async getTurnByIdUser(id) {
+  /**
+   * Parses content of query response body to extract a list of shifts 
+   */
+  parseAllocatedShifts(body){
+    let turni = [];
+
+    for (let i = 0; i < body.length; i++) {
+        let turno = new Object();
+        turno.id = body[i].id;
+        turno.startDate = body[i].inizio;
+        turno.endDate =body[i].fine;
+
+        let utenti_guardia = [];
+        let utenti_reperibili = [];
+
+
+      for (let j = 0; j < body[i].utentiDiGuardia.length; j++) {
+           utenti_guardia[j] = body[i].utentiDiGuardia[j].id;
+        }
+
+      for (let j = 0; j < body[i].utentiReperibili.length; j++) {
+          utenti_reperibili[j] = body[i].utentiReperibili[j].id;
+      }
+
+      turno.title= "Turno in "+  body[i].servizio.nome;
+
+      turno.utenti_guardia = utenti_guardia;
+      turno.utenti_reperibili = utenti_reperibili;
+
+      turno.tipologia = body[i].tipologiaTurno;
+
+      turni[i] = turno;
+
+    }
+    
+    return turni;
+}  
+  
+  async getTurnByIdUser(id) {
         const response = await fetch('/api/assegnazioneturni/utente_id='+id);
         const body = await response.json();
 
-        const turni = [];
-
-        for (let i = 0; i < body.length; i++) {
-            const turno = new Object();
-            turno.startDate = body[i].inizio;
-            turno.endDate =body[i].fine;
-            turno.id = body[i].id;
-            turno.tipologia = body[i].tipologiaTurno;
-            turni[i]=turno;
-            for (let j = 0; j < body[i].utentiDiGuardia.length; j++) {
-             if (id==body[i].utentiDiGuardia[j].id) {
-               turno.title= "Turno in "+  body[i].servizio.nome ;
-               turni[i].turno="GUARDIA"
-             }
-            }
-            for (let j = 0; j < body[i].utentiReperibili.length; j++) {
-              if (id==body[i].utentiReperibili[j].id) {
-                turno.title= "Turno in "+  body[i].servizio.nome;
-                turni[i].turno="REPERIBILITA'"
-            }
-          }
-        }
-
-        return turni;
+        return this.parseAllocatedShifts(body);
 
     }
 
@@ -36,41 +51,7 @@ export  class AssegnazioneTurnoAPI {
         const response = await fetch('/api/assegnazioneturni/');
         const body = await response.json();
 
-        console.log(body)
-
-        let turni = [];
-
-        for (let i = 0; i < body.length; i++) {
-            let turno = new Object();
-            turno.id = body[i].id;
-            turno.startDate = body[i].inizio;
-            turno.endDate =body[i].fine;
-
-            let utenti_guardia = [];
-            let utenti_reperibili = [];
-
-
-          for (let j = 0; j < body[i].utentiDiGuardia.length; j++) {
-               utenti_guardia[j] = body[i].utentiDiGuardia[j].id;
-            }
-
-          for (let j = 0; j < body[i].utentiReperibili.length; j++) {
-              utenti_reperibili[j] = body[i].utentiReperibili[j].id;
-          }
-
-          turno.title= "Turno in "+  body[i].servizio.nome;
-
-          turno.utenti_guardia = utenti_guardia;
-          turno.utenti_reperibili = utenti_reperibili;
-
-          turno.tipologia = body[i].tipologiaTurno;
-
-          turni[i] = turno;
-
-        }
-        
-        return turni;
-
+        return this.parseAllocatedShifts(body);
     }
 
   }
--- frontend/src/views/SingleScheduleView.js
+++ frontend/src/views/SingleScheduleView.js
@@ -1,184 +1,24 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
 import { AssegnazioneTurnoAPI } from '../API/AssegnazioneTurnoAPI';
-import {ViewState,} from '@devexpress/dx-react-scheduler';
 import { UtenteAPI } from '../API/UtenteAPI';
 
-import {
-  Scheduler,
-  Appointments,
-  WeekView,
-  AppointmentTooltip,
-  CurrentTimeIndicator,
-  DayView,
-  ViewSwitcher,
-  TodayButton,
-  DateNavigator,
-  Toolbar,
-  MonthView, Resources,
-} from '@devexpress/dx-react-scheduler-material-ui';
-import styled from "@emotion/styled";
-import {
-  Button,
-} from "@mui/material";
+import ScheduleView from "./ScheduleView.js"
 
-
-const PREFIX = 'Demo';
-// #FOLD_BLOCK
-const classes = {
-  flexibleSpace: `${PREFIX}-flexibleSpace`,
-  textField: `${PREFIX}-textField`,
-  locationSelector: `${PREFIX}-locationSelector`,
-  button: `${PREFIX}-button`,
-  selectedButton: `${PREFIX}-selectedButton`,
-  longButtonText: `${PREFIX}-longButtonText`,
-  shortButtonText: `${PREFIX}-shortButtonText`,
-  title: `${PREFIX}-title`,
-  textContainer: `${PREFIX}-textContainer`,
-  time: `${PREFIX}-time`,
-  text: `${PREFIX}-text`,
-  container: `${PREFIX}-container`,
-  weekendCell: `${PREFIX}-weekendCell`,
-  weekEnd: `${PREFIX}-weekEnd`,
-};
-// #FOLD_BLOCK
-const StyledAppointmentsAppointmentContent = styled(Appointments.AppointmentContent)(() => ({
-  [`& .${classes.title}`]: {
-    fontWeight: 'bold',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-  },
-  [`& .${classes.textContainer}`]: {
-    lineHeight: 1,
-    whiteSpace: 'pre-wrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    width: '100%',
-  },
-  [`& .${classes.time}`]: {
-    display: 'inline-block',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-  },
-  [`& .${classes.text}`]: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-  },
-  [`& .${classes.container}`]: {
-    width: '100%',
-  },
-}));
-
-const AppointmentContent = ({
-                              data, formatDate, ...restProps
-                            }) => (
-  <StyledAppointmentsAppointmentContent {...restProps} formatDate={formatDate} data={data}>
-    <div className={classes.container}>
-      <div className={classes.title}>
-        {data.title}
-      </div>
-      <div className={classes.textField}>
-        {data.turno}
-      </div>
-      <div className={classes.textContainer}>
-        {data.tipologia}
-      </div>
-
-      <div className={classes.textContainer}>
-        <div className={classes.time}>
-          {formatDate(data.startDate, { hour: 'numeric', minute: 'numeric' })}
-        </div>
-        <div className={classes.time}>
-          {' - '}
-        </div>
-        <div className={classes.time}>
-          {formatDate(data.endDate, { hour: 'numeric', minute: 'numeric' })}
-        </div>
-      </div>
-    </div>
-  </StyledAppointmentsAppointmentContent>
-);
-
-export default class SingleScheduleView extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data:[],
-    };
-
-  }
-
+/**
+ * A view showing shift scheduling of a single user
+ */
+export default class SingleScheduleView extends ScheduleView {
 
   async componentDidMount() {
     let apiTurno = new AssegnazioneTurnoAPI();
     let turni = await apiTurno.getTurnByIdUser('1');
-    let utenteAPI = new UtenteAPI();
+    
+    // FIXME: Only colleagues should be queried here, not all users
+    let utenti = await (new UtenteAPI()).getAllUser();
 
-    this.setState(
-      {
-        data:turni,
-      })
-    this.changeMainResource = this.changeMainResource.bind(this);
-  }
-
-  changeMainResource(mainResourceName) {
-    this.setState({ mainResourceName });
+    super.componentDidMount(turni, utenti);
   }
 
   render() {
-    const { data, resources } = this.state;
-
-    return (
-      <Paper>
-        <Scheduler
-          locale={"it-IT"}
-          data={data}
-          height={660}
-          firstDayOfWeek={1}
-          >
-          <ViewState/>
-          <WeekView
-            displayName="Settimanale"
-            startDayHour={0}
-            endDayHour={24}
-            cellDuration={120}
-          />
-          <DayView
-            displayName="Giornaliero"
-            startDayHour={0}
-            endDayHour={24}
-            cellDuration={120}
-          />
-          <MonthView displayName="Mensile" />
-          <Appointments appointmentContentComponent={AppointmentContent} />
-          <Resources
-            data={resources}
-          />
-          <Toolbar />
-          <DateNavigator/>
-          <TodayButton  buttonComponent={(props) => {
-            return (
-              <Button onClick={() => props.setCurrentDate(new Date())}>
-                Oggi
-              </Button>
-            );
-          }}/>
-          <AppointmentTooltip
-            showCloseButton
-          />
-          <ViewSwitcher />
-          <CurrentTimeIndicator
-            shadePreviousAppointments={true}
-            shadePreviousCells={true}
-            updateInterval={60000}
-          />
-        </Scheduler>
-      </Paper>
-
-    );
+    return super.render();
   }
 }
