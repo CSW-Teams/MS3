a8776bd5a59296915bcb7c7c2d421fc1f01d59a3
Federica <federica_vil@yahoo.it>
2023-01-12 21:47:21 +0100

    fix #109

--- src/main/java/org/cswteams/ms3/control/vincoli/VincoloAssegnazioneTurnoTurno.java
+++ src/main/java/org/cswteams/ms3/control/vincoli/VincoloAssegnazioneTurnoTurno.java
@@ -2,11 +2,17 @@ package org.cswteams.ms3.control.vincoli;
 
 import org.cswteams.ms3.entity.AssegnazioneTurno;
 
+import java.time.LocalDate;
+
 public abstract class VincoloAssegnazioneTurnoTurno implements Vincolo{
 
     protected boolean verificaContiguitàAssegnazioneTurni(AssegnazioneTurno turno1, AssegnazioneTurno turno2) {
-        // TODO: Bisogna vedere com'è la nuova modellazione del turno notturno e modificare questo controllo sulle date di conseguenza
-        if(turno1.getData().isEqual(turno2.getData())){
+        LocalDate data1;
+        if(turno1.getTurno().isGiornoSuccessivo()){
+            // Nel caso di turno notturno
+            data1 = turno1.getData().plusDays(1);
+        } else data1 = turno1.getData();
+        if(data1.isEqual(turno2.getData())){
             return turno1.getTurno().getOraFine().equals(turno2.getTurno().getOraInizio());
         }
         else return false;
