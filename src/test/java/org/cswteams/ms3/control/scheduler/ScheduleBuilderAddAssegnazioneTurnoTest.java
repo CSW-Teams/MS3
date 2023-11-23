580feef0994f7dae2edb4e10a11c8d27f3a05ea6
simone-staccone <simone.staccone@virgilio.it>
2023-11-23 13:12:05 +0100

    [TEST] Added tests for ScheduleBuilder build() method

---
+++ src/test/java/org/cswteams/ms3/control/scheduler/ScheduleBuilderAddAssegnazioneTurnoTest.java
@@ -0,0 +1,13 @@
+package org.cswteams.ms3.control.scheduler;
+
+import org.junit.jupiter.api.Test;
+
+public class ScheduleBuilderAddAssegnazioneTurnoTest extends ScheduleBuilderTest{
+
+    @Test
+    @Override
+    void addAssegnazioneTurno() {
+        super.addAssegnazioneTurno();
+        /* TODO */
+    }
+}
