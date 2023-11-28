820cec09aa63b1f662bc1e6ce4342c7fd2e34943
kobero98 <matteo.fede@live.it>
2023-11-28 10:14:16 +0000

    Correzzione URL utilizzato dalla classe HolidayRestEndpoint #255

--- src/main/java/org/cswteams/ms3/rest/HolidayRestEndpoint.java
+++ src/main/java/org/cswteams/ms3/rest/HolidayRestEndpoint.java
@@ -1,6 +1,5 @@
 package org.cswteams.ms3.rest;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,7 +14,6 @@ import org.cswteams.ms3.exception.CalendarServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,7 +33,7 @@ public class HolidayRestEndpoint {
     private CalendarSetting setting;
 
     public HolidayRestEndpoint() {
-    	this.setting = new CalendarSetting("https://date.nager.at/api/v3/publicholidays");
+    	this.setting = new CalendarSetting("https://date.nager.at/api/v3/PublicHolidays");
     }
     
     
