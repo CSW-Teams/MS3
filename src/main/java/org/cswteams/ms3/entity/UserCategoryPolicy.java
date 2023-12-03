31eb20949c7fffb1ae0fc004dc910d0646ef8a91
Danilo D'Amico <danilodamico1@gmail.com>
2023-12-03 22:16:06 +0100

    trying to merge pull request step 2

--- src/main/java/org/cswteams/ms3/entity/UserCategoryPolicy.java
+++ src/main/java/org/cswteams/ms3/entity/UserCategoryPolicy.java
@@ -1,44 +1,55 @@
 package org.cswteams.ms3.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
-import lombok.NonNull;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.cswteams.ms3.dao.CategorieDao;
 
 /**
  * Modella se una categoria utente è vietata o necessaria per un turno
  */
 @Entity
-@Data
- public class UserCategoryPolicy {
-    
-    @Id
-    @GeneratedValue
-    private Long id;
+@Getter
+@Setter
+@EqualsAndHashCode
+public class UserCategoryPolicy {
 
-    @ManyToOne
-    @NonNull
-    private Categoria categoria;
+   @Id
+   @GeneratedValue
+   private Long id;
 
-    @ManyToOne
-    @JsonIgnore
-    @NonNull
-    private Turno turno;
-    
-    /** 
-     * La registrazione di questa policy nel db implica che tale categoria è da escludere
-     * o da includere per un turno. Se così non fosse, non sarebbe stata registrata.
-     * 
-     * 
+   @ManyToOne
+   @NonNull
+   private Categoria categoria;
+
+   @ManyToOne
+   @JsonIgnore
+   @NonNull
+   private Turno turno;
+
+   /**
+    * La registrazione di questa policy nel db implica che tale categoria è da escludere
+    * o da includere per un turno. Se così non fosse, non sarebbe stata registrata.
+    *
+    *
     */
-    @NonNull
-    @Enumerated(value = EnumType.STRING)
-    private UserCategoryPolicyValue policy;
+   @NonNull
+   @Enumerated(value = EnumType.STRING)
+   private UserCategoryPolicyValue policy;
 
-    public UserCategoryPolicy(){
+   public UserCategoryPolicy(){
 
-    }
+   }
 
    public UserCategoryPolicy(Categoria categoria, Turno turno, UserCategoryPolicyValue value){
       this.categoria = categoria;
