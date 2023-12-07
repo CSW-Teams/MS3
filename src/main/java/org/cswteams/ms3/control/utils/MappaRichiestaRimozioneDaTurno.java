efa93acd5f9b57af6f9b4630e80eedad7306208c
massimostanzione <massimo.stanzione@hotmail.it>
2023-12-07 18:05:22 +0100

    ref. #318, task B3

---
+++ src/main/java/org/cswteams/ms3/control/utils/MappaRichiestaRimozioneDaTurno.java
@@ -0,0 +1,28 @@
+package org.cswteams.ms3.control.utils;
+
+import org.cswteams.ms3.dto.RichiestaRimozioneDaTurnoDTO;
+import org.cswteams.ms3.entity.RichiestaRimozioneDaTurno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MappaRichiestaRimozioneDaTurno {
+
+    public static RichiestaRimozioneDaTurnoDTO richiestaRimozioneDaTurnoToDTO(RichiestaRimozioneDaTurno richiestaRimozioneDaTurno) {
+        return new RichiestaRimozioneDaTurnoDTO(richiestaRimozioneDaTurno.getUtente().getNome(),
+                richiestaRimozioneDaTurno.getUtente().getCognome(),
+                ConvertitoreData.daStandardVersoTestuale(richiestaRimozioneDaTurno.getUtente().getDataNascita().toString()),
+                richiestaRimozioneDaTurno.getUtente().getRuoloEnum(),
+                richiestaRimozioneDaTurno.getUtente().getStato(),
+                richiestaRimozioneDaTurno.getUtente().getSpecializzazioni(),
+                richiestaRimozioneDaTurno.getDescrizione());
+    }
+
+    public static List<RichiestaRimozioneDaTurnoDTO> richiestaRimozioneDaTurnoEntitytoDTO(List<RichiestaRimozioneDaTurno> richiestaRimozioneDaTurnoList){
+        List<RichiestaRimozioneDaTurnoDTO> richiestaRimozioneDaTurnoDTOS = new ArrayList<>();
+        for (RichiestaRimozioneDaTurno entity: richiestaRimozioneDaTurnoList){
+            richiestaRimozioneDaTurnoDTOS.add(richiestaRimozioneDaTurnoToDTO(entity));
+        }
+        return richiestaRimozioneDaTurnoDTOS;
+    }
+}
