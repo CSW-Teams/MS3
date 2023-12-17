903bbfb8fe9a4be3a1bd34b7fca8ba86b2d7eb9b
simoneb <simonebauco1@gmail.com>
2023-12-17 18:03:19 +0100

    Modifiche all'interfaccia di gestione del ritiro

--- frontend/src/components/common/BottomViewGestisciRitiro.js
+++ frontend/src/components/common/BottomViewGestisciRitiro.js
@@ -2,26 +2,17 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import {useState} from "react";
-import {TurnoAPI} from "../../API/TurnoAPI";
-import {AssegnazioneTurnoAPI} from "../../API/AssegnazioneTurnoAPI";
-import {AppointmentForm} from "@devexpress/dx-react-scheduler-material-ui";
-import {UtenteAPI} from "../../API/UtenteAPI";
 import Typography from "@mui/material/Typography";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import {Checkbox} from "@mui/material";
+import {AppBar, Checkbox, Toolbar} from "@mui/material";
+import ClearIcon from '@mui/icons-material/Clear';
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const TemporaryDrawerRetirement = ({request, shifts, users}) => {
   const [open, setOpen] = useState(false);
-  const [selectedReperibili, setSelectedReperibili] = useState([]);
+  const [selectedReperibile, setSelectedReperibile] = useState(null);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -33,27 +24,16 @@ const TemporaryDrawerRetirement = ({request, shifts, users}) => {
   console.log(retiringUser);
 
   const handleCheckboxChange = (userId) => {
-    // Verifica se l'utente è già stato selezionato
-    if (selectedReperibili.includes(userId)) {
-      // Rimuovi l'utente dalla lista dei selezionati
-      setSelectedReperibili((prevSelected) =>
-        prevSelected.filter((id) => id !== userId)
-      );
-    } else {
-      // Aggiungi l'utente alla lista dei selezionati
-      setSelectedReperibili((prevSelected) => [...prevSelected, userId]);
-    }
+    setSelectedReperibile(userId);
   };
 
-  const handleApprove = () => {
-    // Logica per l'approvazione
-    console.log('Approvato:', selectedReperibili);
+  const handleApprove = (idReperibile) => {
+    console.log("Approvato ritiro dell'utente con id", retiringUser.id, ". Verrà sostituito dall'utente con id", selectedReperibile);
     handleClose();
   };
 
   const handleReject = () => {
-    // Logica per il rifiuto
-    console.log('Rifiutato:', selectedReperibili);
+    console.log("Ritiro rifiutato");
     handleClose();
   };
 
@@ -63,33 +43,76 @@ const TemporaryDrawerRetirement = ({request, shifts, users}) => {
         Gestisci richiesta
       </Button>
 
+
       <Drawer anchor="bottom" open={open} onClose={handleClose}>
+
+        <AppBar position="static" color="transparent">
+          <Toolbar>
+            <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'center' }}>
+              <Typography variant="h5" component="div" sx={{ marginLeft: '20px' }}>
+                Gestione richiesta di ritiro
+              </Typography>
+            </Box>
+            <IconButton color="inherit" onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
         <div style={{textAlign: 'center', padding: '20px'}}>
-          <Typography variant="h5">
-            {retiringUser.text} vuole ritirarsi
-          </Typography>
 
-          <Typography variant="h6">Utenti Reperibili:</Typography>
+          <Box
+            marginBottom={2}
+            marginTop={2}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography variant="h6">
+              {retiringUser.text} ha richiesto di ritirarsi dal turno.
+            </Typography>
+          </Box>
 
-          {shift.utenti_reperibili.map((utente) => (
-            <FormControlLabel
-              key={utente.id}
-              control={
-                <Checkbox
-                  checked={selectedReperibili.includes(utente.id)}
-                  onChange={() => handleCheckboxChange(utente.id)}
-                />
-              }
-              label={`${utente.nome} ${utente.cognome}`}
-            />
-          ))}
+          <Box
+            marginBottom={2}
+            marginTop={2}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography variant="h6">Seleziona un utente reperibile per la sostituzione:</Typography>
+          </Box>
 
-          <Box mt={2}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            {shift.utenti_reperibili.map((utente) => (
+              <FormControlLabel
+                key={utente.id}
+                control={
+                  <Checkbox
+                    checked={selectedReperibile === utente.id}
+                    onChange={() => handleCheckboxChange(utente.id)}
+                  />
+                }
+                label={`${utente.nome} ${utente.cognome}`}
+              />
+            ))}
+          </Box>
+
+          <Box
+            mt={4}
+          >
             <Button
               variant="contained"
               color="success"
               onClick={handleApprove}
-              disabled={selectedReperibili.length === 0}
+              disabled={selectedReperibile === null}
             >
               Approva
             </Button>
