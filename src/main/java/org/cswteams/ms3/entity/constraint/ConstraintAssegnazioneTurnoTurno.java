e61b7a769c4b455f1b169dd385e6b14d45c7a53c
Fanfarillo <matteo.fanfarillo99@gmail.com>
2023-12-27 23:08:44 +0100

    [REFACTOR] Started refactor of Constraint entities

--- src/main/java/org/cswteams/ms3/entity/constraint/ConstraintAssegnazioneTurnoTurno.java
+++ src/main/java/org/cswteams/ms3/entity/constraint/ConstraintAssegnazioneTurnoTurno.java
@@ -1,5 +1,6 @@
 package org.cswteams.ms3.entity.constraint;
 
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 
@@ -13,54 +14,66 @@ import java.util.List;
 public abstract class ConstraintAssegnazioneTurnoTurno extends Constraint {
 
     /**
-     * Controlla se aTurno2 inizia nello stesso orario in cui finisce aTurno1
+     * This method checks if cShift2 starts at the same hour which cShift1 finishes at.
+     * @param cShift1 First concrete shift in temporal order
+     * @param cShift2 Second concrete shift in temporal order
+     * @return Boolean that represents if the check succeeded
      */
-    protected boolean verificaContiguitàAssegnazioneTurni(ConcreteShift aTurno1, ConcreteShift aTurno2) {
+    protected boolean verificaContiguitàAssegnazioneTurni(ConcreteShift cShift1, ConcreteShift cShift2) {
 
-        return verificaContiguitàAssegnazioneTurni(aTurno1, aTurno2, ChronoUnit.MINUTES, 0);
+        return verificaContiguitàAssegnazioneTurni(cShift1, cShift2, ChronoUnit.MINUTES, 0);
     }
 
     /**
-     * Controlla se aTurno2 inizia nello stesso orario in cui finisce aTurno1,
-     * ammettendo un errore pari a delta
-     * Le assegnazioni turno non devono essere sovrapposte temporalmente.
-     * @param aTurno1
-     * @param aTurno2 deve essere successivo temporalmente ad aTurno1
-     * @param tu unità di misura temporale per delta (minuti, anni, ...)
-     * @param delta numero di unità temporali tollerabili per considerare i turni contigui
-     * @return
+     * This method checks if cShift2 starts at the same hour which cShift1 finishes at plus a delay equal to delta.
+     * The concrete shifts shall not overlap.
+     * @param cShift1 First concrete shift in temporal order
+     * @param cShift2 Second concrete shift in temporal order
+     * @param tu Measure unit for param delta (e.g. minutes, hours)
+     * @param delta max number of tolerable temporal units in order to consider cShift1 and cShift2 contiguous
+     * @return Boolean that represents if the check succeeded
      */
-    protected boolean verificaContiguitàAssegnazioneTurni(ConcreteShift aTurno1, ConcreteShift aTurno2, TemporalUnit tu, long delta){
+    protected boolean verificaContiguitàAssegnazioneTurni(ConcreteShift cShift1, ConcreteShift cShift2, TemporalUnit tu, long delta){
 
-        /*
-        TODO: Refactor using EpochDAy format
-        LocalDateTime aTurno1Start = aTurno1.getData().atTime(aTurno1.getShift().getStartTime());
-        LocalDateTime aTurno1End = aTurno1Start.plus(aTurno1.getShift().getDuration());
+        LocalDate cShift1Date = Instant.ofEpochMilli(cShift1.getDate()).atZone(ZoneId.systemDefault()).toLocalDate();   //conversion Epoch -> LocalDate of cShift1.getDate()
+        LocalDateTime cShift1Start = cShift1Date.atTime(cShift1.getShift().getStartTime());
+        LocalDateTime cShift1End = cShift1Start.plus(cShift1.getShift().getDuration());
 
-        LocalDateTime aTurno2Start = aTurno2.getData().atTime(aTurno2.getShift().getStartTime());
-        LocalDateTime aTurno2End = aTurno2Start.plus(aTurno2.getShift().getDuration());
+        LocalDate cShift2Date = Instant.ofEpochMilli(cShift2.getDate()).atZone(ZoneId.systemDefault()).toLocalDate();   //conversion Epoch -> LocalDate of cShift2.getDate()
+        LocalDateTime cShift2Start = cShift2Date.atTime(cShift2.getShift().getStartTime());
+        LocalDateTime cShift2End = cShift2Start.plus(cShift2.getShift().getDuration());
 
-        if (aTurno1Start.isBefore(aTurno2Start)){
-            return Math.abs(aTurno1End.until(aTurno2Start, tu)) <= delta;
+        if (cShift1Start.isBefore(cShift2Start)) {
+            return Math.abs(cShift1End.until(cShift2Start, tu)) <= delta;
         }
         else {
-            return Math.abs(aTurno2End.until(aTurno1Start, tu)) <= delta;
-        }*/
+            return Math.abs(cShift2End.until(cShift1Start, tu)) <= delta;
+        }
         return false;
+
     }
 
-    protected int getAssegnazioneTurnoPrecedenteIdx(List<ConcreteShift> turniAssegnati, ConcreteShift turnoDaAssegnare){
-                /*
-        TODO: Refactor using EpochDAy format
-        for(int i = 0; i < turniAssegnati.size(); i++){
-            if(turniAssegnati.get(i).getData().isAfter(turnoDaAssegnare.getData()) || turniAssegnati.get(i).getData().isEqual(turnoDaAssegnare.getData())){
-                if(turniAssegnati.get(i).getShift().getStartTime().isAfter(turnoDaAssegnare.getShift().getStartTime()) || turniAssegnati.get(i).getShift().getStartTime().equals(turnoDaAssegnare.getShift().getStartTime())) {
+    /**
+     * This method returns the concrete shift that is before a new concrete shift passed as parameter.
+     * @param concreteShifts List of all the existent concrete shifts
+     * @param concreteShiftToBeAssigned The new concrete shift to be assigned
+     * @return The existent concrete shift that is before concreteShiftToBeAssigned
+     */
+    protected int getAssegnazioneTurnoPrecedenteIdx(List<ConcreteShift> concreteShifts, ConcreteShift concreteShiftToBeAssigned){
+
+        for(int i = 0; i < concreteShifts.size(); i++){
+            //conversion Epoch -> LocalDate of concreteShifts.get(i).getDate() and concreteShiftToBeAssigned.getDate()
+            LocalDate cShiftDate = Instant.ofEpochMilli(concreteShifts.get(i).getDate()).atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate cShiftDateToBeAssignedDate = Instant.ofEpochMilli(concreteShiftToBeAssigned.getDate()).atZone(ZoneId.systemDefault()).toLocalDate();
+
+            if(cShiftDate.isAfter(cShiftDateToBeAssignedDate) || cShiftDate.isEqual(cShiftDateToBeAssignedDate)){
+                if(concreteShifts.get(i).getShift().getStartTime().isAfter(concreteShiftToBeAssigned.getShift().getStartTime()) || concreteShifts.get(i).getShift().getStartTime().equals(concreteShiftToBeAssigned.getShift().getStartTime())) {
                     return i - 1;
                 }
             }
         }
-        return turniAssegnati.size()-1;*/
-        return 0;
+        return concreteShifts.size() - 1;
+
     }
 
 }
