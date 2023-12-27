f7bafe034c5e43d75dd3b563c0b0b3db93593031
massimostanzione <massimo.stanzione@hotmail.it>
2023-12-27 18:32:42 +0100

    ref. #246, RequestRemovalFromConcreteShift

--- src/main/java/org/cswteams/ms3/control/concreteShift/IConcreteShiftController.java
+++ src/main/java/org/cswteams/ms3/control/concreteShift/IConcreteShiftController.java
@@ -3,8 +3,10 @@ package org.cswteams.ms3.control.concreteShift;
 import org.cswteams.ms3.dto.ConcreteShiftDTO;
 import org.cswteams.ms3.dto.RegistraAssegnazioneTurnoDTO;
 import org.cswteams.ms3.entity.ConcreteShift;
+import org.cswteams.ms3.entity.Doctor;
 import org.cswteams.ms3.exception.AssegnazioneTurnoException;
 
+import javax.validation.constraints.NotNull;
 import java.text.ParseException;
 import java.util.Set;
 
@@ -17,7 +19,16 @@ public interface IConcreteShiftController {
 
     ConcreteShift leggiTurnoByID(long idAssegnazione);
 
-    //AssegnazioneTurno sostituisciUtenteAssegnato(AssegnazioneTurno assegnazioneTurno, Utente utenteSostituendo, Utente utenteSostituto) throws AssegnazioneTurnoException;
-
-
+    /**
+     * Substitute a doctor assigned to a concrete shift (i.e. is either <i>on duty</i> or <i>on call</i> for it)
+     * with a doctor that is <i>on call</i> for it.
+     *
+     * @param concreteShift    the concrete shift the substitution is referring to
+     * @param requestingDoctor doctor requesting the substitution
+     * @param substituteDoctor <i>on-call</i>-doctor that will substitute <code>requestingDoctor</code>
+     * @return object related to the concrete shift
+     * @throws AssegnazioneTurnoException if the requesting doctor is neither <i>on duty</i> nor <i>on call</i>,
+     *                                    or if the substitute doctor is not <i>on call</i> for the concrete shift.
+     */
+    ConcreteShift substituteAssignedDoctor(@NotNull ConcreteShift concreteShift, @NotNull Doctor requestingDoctor, @NotNull Doctor substituteDoctor) throws AssegnazioneTurnoException;
 }
