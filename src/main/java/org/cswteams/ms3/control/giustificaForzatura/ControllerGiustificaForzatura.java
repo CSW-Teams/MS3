29cb05f620e4483eee7e830087de737a0dece70b
massimostanzione <massimo.stanzione@hotmail.it>
2023-12-27 19:24:48 +0100

    ref. #246, GiustificazioneForzatura

--- src/main/java/org/cswteams/ms3/control/giustificaForzatura/ControllerGiustificaForzatura.java
+++ src/main/java/org/cswteams/ms3/control/giustificaForzatura/ControllerGiustificaForzatura.java
@@ -1,17 +1,26 @@
 package org.cswteams.ms3.control.giustificaForzatura;
 
+import org.cswteams.ms3.dao.DoctorDAO;
 import org.cswteams.ms3.dao.GiustificazioneFozaturaDAO;
 import org.cswteams.ms3.dao.LiberatoriaDAO;
-import org.cswteams.ms3.dao.DoctorDAO;
+import org.cswteams.ms3.dto.DoctorDTO;
 import org.cswteams.ms3.dto.GiustificazioneForzaturaVincoliDTO;
+import org.cswteams.ms3.dto.MedicalServiceDTO;
 import org.cswteams.ms3.entity.Doctor;
+import org.cswteams.ms3.entity.GiustificazioneForzaturaVincoli;
+import org.cswteams.ms3.entity.MedicalService;
 import org.cswteams.ms3.entity.Waiver;
+import org.cswteams.ms3.exception.DatabaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ControllerGiustificaForzatura implements IControllerGiustificaForzatura {
@@ -26,14 +35,20 @@ public class ControllerGiustificaForzatura implements IControllerGiustificaForza
     DoctorDAO doctorDAO;
 
     @Override
-    public void saveGiustificazione(GiustificazioneForzaturaVincoliDTO giustificazioneForzaturaVincoliDTO) {
-        //giustificazioneFozaturaDao.save(giustificazioneForzaturaVincoli);
+    public void saveGiustificazione(GiustificazioneForzaturaVincoliDTO giustificazioneForzaturaVincoliDTO) throws DatabaseException {
         Doctor giustificatore = doctorDAO.findById(Long.parseLong(giustificazioneForzaturaVincoliDTO.getUtenteGiustificatoreId()));
-        //GiustificazioneForzaturaVincoli giustificazioneForzaturaVincoli = new GiustificazioneForzaturaVincoli(giustificazioneForzaturaVincoliDTO.getMessage(),giustificazioneForzaturaVincoliDTO.getTipologiaTurno(), MappaServizio.servizioDTOtoEntity(giustificazioneForzaturaVincoliDTO.getServizio()), giustificazioneForzaturaVincoliDTO.getGiorno(), MappaUtenti.utenteDTOtoEntity(giustificazioneForzaturaVincoliDTO.getUtentiAllocati()), giustificatore);
-
-        //giustificazioneFozaturaDao.save(giustificazioneForzaturaVincoli);
+        GiustificazioneForzaturaVincoli giustificazioneForzaturaVincoli =
+                new GiustificazioneForzaturaVincoli(giustificazioneForzaturaVincoliDTO.getMessage(),
+                        giustificazioneForzaturaVincoliDTO.getTimeSlot(),
+                        buildServiceDTO(giustificazioneForzaturaVincoliDTO.getServizio()),
+                        LocalDate.of(
+                                giustificazioneForzaturaVincoliDTO.getGiorno(),
+                                giustificazioneForzaturaVincoliDTO.getMese(),
+                                giustificazioneForzaturaVincoliDTO.getAnno()),
+                        convertDTOSetToEntitySet(giustificazioneForzaturaVincoliDTO.getUtentiAllocati()),
+                        giustificatore);
+        giustificazioneFozaturaDao.save(giustificazioneForzaturaVincoli);
     }
-
 
     @Override
     public Waiver saveDelibera(MultipartFile file) throws IOException {
@@ -47,5 +62,45 @@ public class ControllerGiustificaForzatura implements IControllerGiustificaForza
         return liberatoriaDao.findDeliberaByName(filename);
     }
 
+    public static MedicalService buildServiceDTO(MedicalServiceDTO dto) {
+        return new MedicalService(dto.getMansioni(), dto.getNome());
+    }
 
+    private Doctor _getDoctor(Long doctorId) throws DatabaseException {
+        Optional<Doctor> doctor = doctorDAO.findById(doctorId);
+        if (doctor.isEmpty()) {
+            throw new DatabaseException("Doctor not found for id = " + doctorId);
+        }
+        return doctor.get();
+    }
+
+    public Set<Doctor> convertDTOSetToEntitySet(Set<DoctorDTO> dtoSet) throws DatabaseException {
+        Set<Doctor> entitySet = new HashSet<>();
+        for (DoctorDTO dto : dtoSet) {
+
+            /*
+             Get the Doctor entities directly from the db, instead of by using the
+             (pontentially) fewer/incomplete DTO parameters.
+             Moreover, DTO does not include the last parameter "roles" of the entity
+             so it is not possible to build a Doctor entity
+            */
+            entitySet.add(_getDoctor(dto.getId()));
+
+            // old code temporarily below, for reference
+            /*
+            entitySet.add(new Doctor(
+                    dto.getName(),
+                    dto.getLastname(),
+                    dto.getCodiceFiscale(),
+                    dto.getDataNascita(),
+                    dto.getEmail(),
+                    dto.getPassword(),
+                    dto.getSeniority(),
+                    dto.getActor() // <-- entity/DTO mismatch!
+                    )
+            );
+            */
+        }
+        return entitySet;
+    }
 }
