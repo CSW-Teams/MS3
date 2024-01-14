97b12516bcf42ac1980e4564c27c0214053800b8
kobero98 <matteo.fede@live.it>
2024-01-14 22:50:08 +0000

    NotificationDAO

--- src/main/java/org/cswteams/ms3/dao/NotificationDAO.java
+++ src/main/java/org/cswteams/ms3/dao/NotificationDAO.java
@@ -1,4 +1,14 @@
 package org.cswteams.ms3.dao;
 
-public interface NotificationDAO {
+import org.cswteams.ms3.entity.Notification;
+import org.cswteams.ms3.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface NotificationDAO extends JpaRepository<Notification,Long> {
+    Set<Notification> findByUserAndStatus(User user, boolean status);
 }
+
