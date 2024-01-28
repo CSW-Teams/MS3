87a729376a70c48b0d228c287f55a9e7db60b072
massimostanzione <massimo.stanzione@hotmail.it>
2024-01-28 22:37:50 +0100

    ref. #463, backend, pkg enums

--- src/main/java/org/cswteams/ms3/enums/Seniority.java
+++ src/main/java/org/cswteams/ms3/enums/Seniority.java
@@ -1,5 +1,23 @@
 package org.cswteams.ms3.enums;
 
+/**
+ * This enumeration provides all the possible values for specification of a <i>Doctor</i>'s seniority.
+ *
+ * @see org.cswteams.ms3.entity.Doctor
+ */
 public enum Seniority {
-    STRUCTURED, SPECIALIST_JUNIOR,SPECIALIST_SENIOR
+    /**
+     * Structured doctor.
+     */
+    STRUCTURED,
+
+    /**
+     * First two years-specialist.
+     */
+    SPECIALIST_JUNIOR,
+
+    /**
+     * Specialist after second year
+     */
+    SPECIALIST_SENIOR
 }
--- src/main/java/org/cswteams/ms3/enums/SystemActor.java
+++ src/main/java/org/cswteams/ms3/enums/SystemActor.java
@@ -1,5 +1,12 @@
 package org.cswteams.ms3.enums;
 
+/**
+ * This enumeration provides all the <i>roles</i> that an <code>User</code> can have.
+ *
+ * @see <a href="https://github.com/CSW-Teams/MS3/wiki#attori">Glossary</a>.
+ * @see org.cswteams.ms3.entity.User
+ * @see org.cswteams.ms3.control.actors.ISystemActorController
+ */
 public enum SystemActor {
     CONFIGURATOR, PLANNER, DOCTOR
 }
