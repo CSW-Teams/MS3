51f13595e9ae5a54edb7097cd61b271f732b5766
massimostanzione <massimo.stanzione@hotmail.it>
2024-02-01 17:08:51 +0100

    ref. #469, rebase: linked scheduler factory to REST endpoint

--- src/main/java/org/cswteams/ms3/entity/scheduling/factory/SchedulerControllerFactory.java
+++ src/main/java/org/cswteams/ms3/entity/scheduling/factory/SchedulerControllerFactory.java
@@ -13,8 +13,25 @@ public class SchedulerControllerFactory {
             case SCHEDULER_UFFAPRIORITY:
                 return new SchedulerControllerPriority();
             default:
-                throw new RuntimeException("wut?");
+                throw new RuntimeException("Invalid scheduling algorithm");
         }
     }
 
+    /**
+     * same as before, but more handy for rest endpoints handling
+     * @param algorithm
+     * @return
+     */
+    public ISchedulerController createSchedulerController(int algorithm) {
+        switch (algorithm) {
+            case 1:
+                return new SchedulerControllerUffaPoints();
+            case 2:
+                return new SchedulerControllerPriority();
+            default:
+                // we don't want any trouble
+                return new SchedulerControllerPriority();
+                //throw new RuntimeException("Invalid scheduling algorithm");
+        }
+    }
 }
