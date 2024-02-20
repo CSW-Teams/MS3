f3a71408dc8addc4b5900bb8e42ddcb5ba01b859
Fanfarillo <matteo.fanfarillo99@gmail.com>
2024-02-20 09:58:19 +0100

    [WORK IN PROGRESS] debugging manual adding of concrete shift

--- frontend/src/API/GiustificaForzaturaAPI.js
+++ frontend/src/API/GiustificaForzaturaAPI.js
@@ -1,21 +1,10 @@
 export class GiustificaForzaturaAPI {
 
-  async caricaGiustifica(message, utenteId, turno, utenti, data, servizio) {
-    let giustificazione = {};
-    giustificazione.message = message;
-    giustificazione.utenteGiustificatoreId = utenteId;
-    giustificazione.giorno = data.$d.getDate();
-    giustificazione.mese = data.$d.getMonth()+1;
-    giustificazione.anno = data.$d.getFullYear();
-    giustificazione.utentiAllocati = utenti;
-    giustificazione.tipologiaTurno = turno;
-    giustificazione.servizio = servizio;
-    //giustificazione.liberatoria = liberatoria;
-
+  async caricaGiustifica(params) {
     const requestOptions = {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(giustificazione)
+      body: JSON.stringify(params)
     };
     const url = "/api/justify/uploadJustification";
     const response = await fetch(url, requestOptions);
