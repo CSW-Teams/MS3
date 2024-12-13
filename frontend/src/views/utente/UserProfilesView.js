583d8dff8ac2edb610e0a06f397a6c9aeabefabd
ralisin <buniymassimo@gmail.com>
2024-12-14 00:37:34 +0100

    Renamed in "Utenti" column "attore" in "ruolo"; icon has been added to indicate sort direction and sortability Closes #576 and #589

--- frontend/src/views/utente/UserProfilesView.js
+++ frontend/src/views/utente/UserProfilesView.js
@@ -13,10 +13,8 @@ import {toast} from "react-toastify";
 import {panic} from "../../components/common/Panic";
 
 function defaultComparator(prop1, prop2){
-  if (prop1 < prop2)
-    return -1;
-  if (prop1 > prop2)
-    return 1;
+  if (prop1 < prop2) return -1;
+  if (prop1 > prop2) return 1;
   return 0;
 }
 
@@ -25,7 +23,8 @@ export default class UserProfilesView extends React.Component{
     super(props);
     this.state = {
       utenti: [],
-      orderBy: "name",
+      orderBy: "lastname",
+      orderDirection: "asc",
       comparator: defaultComparator,
       attore : localStorage.getItem("actor"),
     }
@@ -36,13 +35,26 @@ export default class UserProfilesView extends React.Component{
    * Cambia la proprietà degli utenti per cui si vuole ordinare,
    * usando un comparatore di default
    */
-  setOrderBy(userProp){
+  // setOrderBy(userProp){
+  //   this.setState({
+  //     orderBy: userProp,
+  //     comparator: defaultComparator
+  //   })
+  // }
+  setOrderBy(userProp) {
+    const { orderBy, orderDirection } = this.state;
+
+    // Cambia direzione di ordinamento se la colonna è la stessa
+    const newOrderDirection = orderBy === userProp
+      ? (orderDirection === "asc" ? "desc" : "asc")
+      : "asc";
+
     this.setState({
       orderBy: userProp,
-      comparator: defaultComparator
-    })
+      orderDirection: newOrderDirection, // Memorizza la direzione di ordinamento
+      comparator: defaultComparator,
+    });
   }
-
     /**
    * Cambia la proprietà degli utenti per cui si vuole ordinare,
    * specificando un comparatore custom
@@ -70,20 +82,31 @@ export default class UserProfilesView extends React.Component{
 
   }
 
+  getSortIcon(column) {
+    let direction = "fas fa-sort"
+    if (this.state.orderBy === column) direction = this.state.orderDirection === "asc" ? "fas fa-sort-up" : "fas fa-sort-down"
+
+    return <i
+      className={direction}
+      style={{
+        color: this.state.orderBy === column ? "#1a1a1a" : "#e0e0e0",
+        marginLeft: "5px",
+      }}
+    ></i>;
+  }
+
 
   render() {
-
-    // Ordina gli utenti in base alla proprietà specificata.
-    // È possibile specificare la proprietà cliccando sulla colonna corrispondente.
-    this.state.utenti.sort((u1, u2) => {
-
+    const sortedData = [...this.state.utenti].sort((u1, u2) => {
       let p1 = u1[this.state.orderBy];
       let p2 = u2[this.state.orderBy];
 
+      // Determine sorting direction
+      const direction = this.state.orderDirection === "asc" ? 1 : -1;
 
-      return this.state.comparator(p1, p2);
-
-    })
+      // Use comparator and apply the direction
+      return direction * this.state.comparator(p1, p2);
+    });
 
     return(
       <MDBCard>
@@ -102,29 +125,59 @@ export default class UserProfilesView extends React.Component{
                     hover >
             <MDBTableHead color='tempting-azure-gradient' textwhite>
               <tr>
-                <th scope='col' onClick={() => this.setOrderBy("name")} >{t("Name")}</th>
-                <th scope='col' onClick={() => this.setOrderBy("lastname")} >{t("Surname")}</th>
-                <th scope='col' onClick={() => this.setOrderBy("birthday")} >{t("Birthdate")}</th>
-                <th scope='col' onClick={() => this.setOrderBy("systemActors")} >{t("Actor")}</th>
+                <th scope='col' onClick={() => this.setOrderBy("name")} >{t("Name")} {this.getSortIcon("name")}</th>
+                <th scope='col' onClick={() => this.setOrderBy("lastname")} >{t("Surname")} {this.getSortIcon("lastname")}</th>
+                <th scope='col' onClick={() => this.setOrderBy("birthday")} >{t("Birthdate")} {this.getSortIcon("birthday")}</th>
+                <th scope='col' onClick={() => this.setOrderBy("systemActors")} >{t("Actor")} {this.getSortIcon("systemActors")}</th>
                 {this.state.attore!=="PLANNER" && <th scope='col'>{t("Info")}</th>}
                 {this.state.attore==="PLANNER" && <th scope='col'>{t("Modify")}</th>}
 
               </tr>
             </MDBTableHead>
             <MDBTableBody>
-              {this.state.utenti.map((data, key) => {
-                return (
-                  <tr key={key}>
-                    <td>{data.name}</td>
-                    <td>{data.lastname}</td>
-                    <td>{data.birthday}</td>
-                    <td>{data.systemActors.map(actor => t(actor)).join(", ")}</td>
-                    {this.state.attore === "UTENTE" &&
-                      <td><Button className="overlay" variant="primary" href={`/single-user-profile/${data.id}`}><i className="fa fa-id-card"> </i></Button></td>}
-                    {this.state.attore!=="UTENTE" && <td><Button className="overlay" variant="primary" href={`/single-user-profile/${data.id}`}><i className="fas fa-edit fa-lg"> </i></Button></td>}
-                  </tr>
-                )
-              })}
+              {sortedData.map((data, key) => (
+                <tr key={key}>
+                  <td>{data.name}</td>
+                  <td>{data.lastname}</td>
+                  <td>{data.birthday}</td>
+                  <td>{data.systemActors.map((actor) => t(actor)).join(", ")}</td>
+                  {this.state.attore === "UTENTE" && (
+                    <td>
+                      <Button
+                        className="overlay"
+                        variant="primary"
+                        href={`/single-user-profile/${data.id}`}
+                      >
+                        <i className="fa fa-id-card"> </i>
+                      </Button>
+                    </td>
+                  )}
+                  {this.state.attore !== "UTENTE" && (
+                    <td>
+                      <Button
+                        className="overlay"
+                        variant="primary"
+                        href={`/single-user-profile/${data.id}`}
+                      >
+                        <i className="fas fa-edit fa-lg"> </i>
+                      </Button>
+                    </td>
+                  )}
+                </tr>
+              ))}
+              {/*{this.state.utenti.map((data, key) => {*/}
+              {/*  return (*/}
+              {/*    <tr key={key}>*/}
+              {/*      <td>{data.name}</td>*/}
+              {/*      <td>{data.lastname}</td>*/}
+              {/*      <td>{data.birthday}</td>*/}
+              {/*      <td>{data.systemActors.map(actor => t(actor)).join(", ")}</td>*/}
+              {/*      {this.state.attore === "UTENTE" &&*/}
+              {/*        <td><Button className="overlay" variant="primary" href={`/single-user-profile/${data.id}`}><i className="fa fa-id-card"> </i></Button></td>}*/}
+              {/*      {this.state.attore!=="UTENTE" && <td><Button className="overlay" variant="primary" href={`/single-user-profile/${data.id}`}><i className="fas fa-edit fa-lg"> </i></Button></td>}*/}
+              {/*    </tr>*/}
+              {/*  )*/}
+              {/*})}*/}
             </MDBTableBody>
           </MDBTable>
         </MDBCardBody>
