f7c51712cfdbba02533d42c274d1ba2f6550b00c
ralisin <buniymassimo@gmail.com>
2024-12-14 20:01:14 +0100

    Closed #590

--- frontend/src/components/common/BottomViewAddSpecialization.js
+++ frontend/src/components/common/BottomViewAddSpecialization.js
@@ -1,144 +1,116 @@
 import React, {useState} from "react";
-import Button from "@mui/material/Button";
-import Drawer from "@mui/material/Drawer";
-import {AppBar, Checkbox, Toolbar} from "@mui/material";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
+import {
+  AppBar,
+  Autocomplete,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  TextField,
+  Toolbar,
+  Typography
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import {SingleUserProfileAPI} from "../../API/SingleUserProfileAPI";
-import {toast} from "react-toastify";
 import {t} from "i18next";
+import {SingleUserProfileAPI} from "../../API/SingleUserProfileAPI";
 import {panic} from "./Panic";
 
-
-
-export default function DoctorSpecializationAdditionDrawer(props){
+export default function DoctorSpecializationAdditionDrawer(props) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const [checkedSpecializationsCheckBoxes, setCheckedSpecializationsCheckBoxes] = React.useState([]);
 
-  const specializationList = Object.values(props.specializations)
+  // List of specializations already assigned to the user
   const currentSpecializationList = props.updatedSpecializationList;
 
-  function initializeCheckedBoxes(specialization){
-    for(var i=0;i<props.updatedSpecializationList.length;i++){
-          if(specialization === props.updatedSpecializationList[i]){
-            return true;
-          }
-    }
-    return false;
-  }
-  const handleAddSpecializations= (updateFunction,doctorID, specializations,checkedSpecializationsCheckBoxes) => {
+  // List of all available specializations
+  const fullSpecializationList = Object.values(props.specializations)
+
+  const [addedSpecializations, setAddedSpecializations] = useState([]);
+  const handleAddSpecializations = async (updateFunction, doctorID, specializations, checkedSpecializationsCheckBoxes) => {
     let singleUserProfileAPI = new SingleUserProfileAPI();
 
-    for(var i = 0;i<checkedSpecializationsCheckBoxes.length;i++){
-      specializations.push(checkedSpecializationsCheckBoxes[i]);
-    }
+    const updatedSpecializations = [...specializations, ...checkedSpecializationsCheckBoxes];
 
     try {
-      let responseStatus = singleUserProfileAPI.addSpecializations(doctorID, specializations);
+      await singleUserProfileAPI.addSpecializations(doctorID, updatedSpecializations);
+      setOpen(false);
+      updateFunction(updatedSpecializations);
+      setAddedSpecializations([]);
     } catch (err) {
-
-      panic()
-      return
+      console.error("Errore:", err);
+      panic();
     }
-    setOpen(false);
-    updateFunction(specializations);
-    setCheckedSpecializationsCheckBoxes(prev => []);
-
   }
 
+  return (<>
+    <Button
+      onClick={handleOpen}
+      style={{
+        'display': 'block',
+        'margin-left': 'auto',
+        'margin-right': 'auto',
+        'margin-top': '1%',
+        'margin-bottom': '1%'
+      }}
+    >
+      <i className="fa fa-plus" aria-hidden="true"></i>
+    </Button>
 
-  const handleCheckboxChange = (specialization) => {
-    if (!checkedSpecializationsCheckBoxes.includes(specialization)) {
-      setCheckedSpecializationsCheckBoxes(prev => [...prev, specialization]);
-    }else {
-      setCheckedSpecializationsCheckBoxes(checkedSpecializationsCheckBoxes.filter(item => item !== specialization));
-    }
-  };
-
-
-  return (
-    <>
-      <Button
-        onClick = {handleOpen}
-        style   = {{
-          'display'       : 'block',
-          'margin-left'   : 'auto',
-          'margin-right'  : 'auto',
-          'margin-top'    : '1%',
-          'margin-bottom' : '1%'
-        }}
-      >
-          <i className="fa fa-plus" aria-hidden="true"></i>
-      </Button>
-      <Drawer anchor="bottom" open={open} onClose={handleClose}>
-        <AppBar position="static" color="transparent">
-          <Toolbar>
-            <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'center' }}>
-              <Typography variant="h5" component="div" sx={{ marginLeft: '20px' }}>
-                {t('Add Specialization')}
-              </Typography>
-            </Box>
-            <IconButton color="inherit" onClick={handleClose}>
-              <CloseIcon/>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-
-        <div style={{textAlign: 'center', padding: '20px'}}>
-          <Box
-            marginBottom    = {2}
-            marginTop       = {2}
-            display         = "flex"
-            flexDirection   = "column"
-            justifyContent  = "center"
-            alignItems      = "center"
-          >
-            <p>
-            </p>
-            <Typography variant="h6">
-              {t('Select specializations to assign:')}
+    <Drawer anchor="bottom" open={open} onClose={handleClose}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <Box sx={{display: 'flex', flexGrow: 1, justifyContent: 'center'}}>
+            <Typography variant="h5" component="div"
+                        sx={{marginLeft: '20px'}}>
+              {t('Add Specialization')}
             </Typography>
-            {
-                specializationList.map((item) => (
-                      <FormControlLabel
-                          control={
-                            <Checkbox
-                                disabled={initializeCheckedBoxes(item)}
-                                defaultChecked={initializeCheckedBoxes(item)}
-                                onChange={() => handleCheckboxChange(item)}
-                            />
-                          }
-                          label={`${item}`}
-                      />
-                  ))
-            }
           </Box>
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-          </Box>
-          <Box
-            mt={4}
-          >
-            <Button
-              variant="contained"
-              disabled={checkedSpecializationsCheckBoxes.length === 0}
-              onClick={ () => handleAddSpecializations(props.updateFunction, props.doctorID,currentSpecializationList,checkedSpecializationsCheckBoxes) }
-            >
-              {t('Save')}
-            </Button>
-          </Box>
-        </div>
-      </Drawer>
-    </>
-  );
+          <IconButton color="inherit" onClick={handleClose}>
+            <CloseIcon/>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: '20px',
+        marginTop: "10px",
+        marginBottom: "10px"
+      }}>
+        <Typography variant="h6" sx={{mb: 1}}>
+          {t('Select specializations to assign:')}
+        </Typography>
+
+        <Autocomplete
+          multiple
+          options={fullSpecializationList}
+          getOptionDisabled={(option) => currentSpecializationList.includes(option)}
+          filterSelectedOptions
+          sx={{mb: 6, width: 400}}
+          onChange={(event, newValue) => setAddedSpecializations(newValue)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label={t("Specializations")}
+              placeholder={t("Add new specialization")}
+            />
+          )}
+        />
+
+        <Button
+          variant="contained"
+          sx={{mb: 3}}
+          disabled={addedSpecializations.length === 0}
+          onClick={() => handleAddSpecializations(props.updateFunction, props.doctorID, currentSpecializationList, addedSpecializations)}
+        >
+          {t('Save')}
+        </Button>
+      </div>
+    </Drawer>
+  </>);
 
 }
