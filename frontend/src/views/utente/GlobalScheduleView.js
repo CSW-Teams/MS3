ddeac42e7b7a5562e2f310d9d14e91a0298706c2
AlessandroFinocchi <alessandro.22082001@gmail.com>
2024-12-18 11:43:56 +0100

    Changed global shedule view

--- frontend/src/views/utente/GlobalScheduleView.js
+++ frontend/src/views/utente/GlobalScheduleView.js
@@ -4,6 +4,7 @@ import { AssegnazioneTurnoAPI } from '../../API/AssegnazioneTurnoAPI';
 import TemporaryDrawer from "../../components/common/BottomViewAssegnazioneTurno.js";
 import { Stack } from "@mui/system";
 import {panic} from "../../components/common/Panic";
+import {MDBCard, MDBCardBody, MDBContainer} from "mdb-react-ui-kit";
 
 class GlobalScheduleView extends ScheduleView {
 
@@ -23,10 +24,17 @@ class GlobalScheduleView extends ScheduleView {
 
   render(){
     return (
-      <Stack spacing={3}>
-        {localStorage.getItem("actor")!=="DOCTOR" && localStorage.getItem("actor")!=="CONFIGURATOR" && <TemporaryDrawer onPostAssegnazione = {()=>{this.componentDidMount() ;}} ></TemporaryDrawer>}
-        {super.render("global")}
-      </Stack>
+      <MDBContainer fluid className="main-content-container px-4 pb-4 pt-4">
+        <MDBCard alignment='center'>
+          <MDBCardBody>
+            <Stack spacing={3}>
+              {localStorage.getItem("actor")!=="DOCTOR" && localStorage.getItem("actor")!=="CONFIGURATOR" && <TemporaryDrawer onPostAssegnazione = {()=>{this.componentDidMount() ;}} ></TemporaryDrawer>}
+              {super.render("global")}
+            </Stack>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
+
       );
   }
 }
