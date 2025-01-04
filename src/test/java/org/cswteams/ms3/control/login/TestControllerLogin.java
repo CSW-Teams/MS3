1d22b346e2dc855284b56170149e54e63bad308a
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-05 00:59:31 +0100

    Recovered tests for login e desiderata.

--- src/test/java/org/cswteams/ms3/control/login/TestControllerLogin.java
+++ src/test/java/org/cswteams/ms3/control/login/TestControllerLogin.java
@@ -1,11 +1,14 @@
 package org.cswteams.ms3.control.login;
 
+import org.cswteams.ms3.dto.login.LoginRequestDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.transaction.Transactional;
 import java.util.stream.Stream;
@@ -13,42 +16,30 @@ import java.util.stream.Stream;
 @SpringBootTest
 @Transactional
 public class TestControllerLogin {
-/*
-    private enum InstanceValidity {
-        VALID,
-        INVALID
-    }
 
     @Autowired
-    private ControllerLogin controllerLogin;
+    private AuthenticationManager authenticationManager;
 
     private static Stream<Arguments> autenticaUtenteParams() {
         return Stream.of(
-                //           loginDTO, exception expected
-                Arguments.of(InstanceValidity.VALID, false),
-                Arguments.of(InstanceValidity.INVALID, true),
-                Arguments.of(null, true)
+                // loginDTO, exception expected
+                Arguments.of(new LoginRequestDTO("**@gmail.com", "passw"), false),
+                Arguments.of(new LoginRequestDTO("email", "password"), true)
         );
     }
 
     @ParameterizedTest
     @MethodSource("autenticaUtenteParams")
-    public void testAutenticaUtente(InstanceValidity validity, boolean exceptionExpected) {
-        LoginRequestDTO loginDTO;
-        if (validity == InstanceValidity.VALID) {
-            loginDTO = new LoginRequestDTO("**@gmail.com", "passw");
-        } else if (validity == InstanceValidity.INVALID) {
-            loginDTO = new LoginRequestDTO("email", "password");
-        } else {
-            loginDTO = null;
-        }
-
+    public void testAutenticaUtente(LoginRequestDTO loginDTO, boolean exceptionExpected) {
         try {
-            controllerLogin.autenticaUtente(loginDTO);
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    loginDTO.getEmail(),
+                    loginDTO.getPassword()
+            ));
             Assertions.assertFalse(exceptionExpected);      // FAIL: non viene comunicata un'eccezione nel caso di utente non esistente
         } catch (Exception e) {
             Assertions.assertTrue(exceptionExpected);
         }
-    }*/
+    }
 
 }
