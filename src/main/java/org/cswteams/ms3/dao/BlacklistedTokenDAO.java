a04017730e6da251202bf505c424cdd14a9ed234
AndreaCantarini <andreacantarini2015@gmail.com>
2026-01-14 23:58:50 +0100

    #621 [BACKEND] Removed unused imports

--- src/main/java/org/cswteams/ms3/dao/BlacklistedTokenDAO.java
+++ src/main/java/org/cswteams/ms3/dao/BlacklistedTokenDAO.java
@@ -1,12 +1,10 @@
 package org.cswteams.ms3.dao;
 
 import org.cswteams.ms3.entity.BlacklistedToken;
-import org.cswteams.ms3.entity.SystemUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface BlacklistedTokenDAO extends JpaRepository<BlacklistedToken, Long> {
