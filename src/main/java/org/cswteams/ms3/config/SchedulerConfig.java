130ba353e4564b402e72d5c72576c517c3062970
AndreaCantarini <andreacantarini2015@gmail.com>
2026-01-14 23:59:54 +0100

    #621 [BACKEND] Added token removal service

---
+++ src/main/java/org/cswteams/ms3/config/SchedulerConfig.java
@@ -0,0 +1,12 @@
+package org.cswteams.ms3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Configuration class for enabling Spring scheduling.
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
